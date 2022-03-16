f33702df87a039d505e9460c4f68632a5b76ede0
Martin Teillet <martin.teillet@hotmail.fr>
2022-03-16 08:25:19 +0100

    Began implementing stretchers for better UI (more place for the viewer), put slider placeholder for the frame number and frame scrolling area + commented the different widgets  Changes to be committed: 	modified:   pyVexr_gui.py

--- pyVexr_gui.py
+++ pyVexr_gui.py
@@ -20,22 +20,40 @@ class MyWidget(QtWidgets.QWidget):
         ####################################
 
         self.file = QtWidgets.QLabel(alignment = QtCore.Qt.AlignCenter)
-        self.file.setText("File Space")
-        
+        # doc for pyqt menu bar :
+        # https://pythonprogramminglanguage.com/pyqt-menu/
+        # https://realpython.com/python-menus-toolbars/
+        self.menuBar = QtWidgets.QMenuBar()
+        self.fileMenu = self.menuBar.addMenu('&File')
+        self.editMenu = self.menuBar.addMenu('&Edit')
+        self.colorspaceMenu = self.menuBar.addMenu('&Colorspace')
+        self.infoMenu = self.menuBar.addMenu('&Info')
+
+        # Channel area - need to replace with a floating window
         self.channels = QtWidgets.QLabel(alignment = QtCore.Qt.AlignCenter)
         self.channels.setText("Exr Channels")
 
+        # Will have to replace the QLabel and QPixmap implementation with a QGraphicsView, in order to enable zoom and pan
         self.img = QtWidgets.QLabel(alignment = QtCore.Qt.AlignCenter)
         self.img.setText("IMG")
         self.load = QtWidgets.QPushButton("Load")
         self.load.clicked.connect(self.function)
 
+        # Version area - Need to replace with a floating window
         self.version = QtWidgets.QLabel(alignment = QtCore.Qt.AlignCenter)
         self.version.setText("Versions")
 
-        self.frameNumber = QtWidgets.QLabel(alignment = QtCore.Qt.AlignCenter)
-        self.frameNumber.setText("Frame Number")
-
+        # Slider for the frame sequence
+        self.frameNumber = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.frameNumber.setValue(101)
+        self.frameNumber.setMinimum(101)
+        self.frameNumber.setMaximum(111)
+        self.frameNumber.setTickPosition(QtWidgets.QSlider.TicksAbove)
+        self.frameNumber.setStyleSheet("color: white")
+        self.frameNumberLabel = QtWidgets.QLabel()
+        self.frameNumberLabel.setText("Frame Number")
+        
+        # Player buttons area
         self.player = QtWidgets.QLabel(alignment = QtCore.Qt.AlignCenter)
         self.player.setText("Player")
 
@@ -46,7 +64,7 @@ class MyWidget(QtWidgets.QWidget):
         self.mainLayout = QtWidgets.QVBoxLayout(self)
         
         self.topBarLayout = QtWidgets.QHBoxLayout()
-        self.topBarLayout.addWidget(self.file)
+        self.topBarLayout.addWidget(self.menuBar)
 
         self.centerLayout = QtWidgets.QHBoxLayout()
         self.channelsLayout = QtWidgets.QVBoxLayout()
@@ -57,17 +75,18 @@ class MyWidget(QtWidgets.QWidget):
         self.versionsLayout = QtWidgets.QVBoxLayout()
         self.versionsLayout.addWidget(self.version)
         self.centerLayout.addLayout(self.channelsLayout)
-        self.centerLayout.addLayout(self.imgLayout)
+        self.centerLayout.addLayout(self.imgLayout, stretch = 1)
         self.centerLayout.addLayout(self.versionsLayout)
 
         self.frameNumLayout = QtWidgets.QHBoxLayout()
-        self.frameNumLayout.addWidget(self.frameNumber)
+        self.frameNumLayout.addWidget(self.frameNumberLabel)
+        self.frameNumLayout.addWidget(self.frameNumber, stretch = 1)
         
         self.playerLayout = QtWidgets.QHBoxLayout()
         self.playerLayout.addWidget(self.player)
 
         self.mainLayout.addLayout(self.topBarLayout)
-        self.mainLayout.addLayout(self.centerLayout)
+        self.mainLayout.addLayout(self.centerLayout, stretch = 1)
         self.mainLayout.addLayout(self.frameNumLayout)
         self.mainLayout.addLayout(self.playerLayout)
 

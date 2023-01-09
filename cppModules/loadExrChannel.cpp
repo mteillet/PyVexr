f2fa7076590373574e9ff052d5b9ca95582df48e
mteillet <martin.teillet@hotmail.fr>
2023-01-09 08:33:44 +0100

    Included pybind11/stl.h module to cpp backend Removed benchmarking for now Still a segmentation fault core dumped error to resolve in the loadExrChannel.cpp Might come from a difference in size between the frame buffer and the read pixels from exr 	modified:   cppModules/loadExrChannel.cpp 	modified:   cppModules/loadExrChannel.cpython-38-x86_64-linux-gnu.so 	modified:   pyVexr_main.py

--- cppModules/loadExrChannel.cpp
+++ cppModules/loadExrChannel.cpp
@@ -1,6 +1,8 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include <pybind11/stl.h>
 #include <iostream>
+#include <vector>
 #include <math.h>
 #include <stdio.h>
 #include <ImathExport.h>
@@ -11,37 +13,55 @@
 #include <OpenEXR/ImfHeader.h>
 #include <OpenEXR/ImfArray.h>
 
+using namespace Imf;
+using namespace Imath;
 namespace py = pybind11;
 
-py::array_t<float> loadExrChan(const std::string& filename, const std::string& selectedChannel){
+std::vector<py::array_t<float>> loadExrChan(const std::string& filename, const std::vector<std::string>& selectedChannel){
 	// Open the .exr file 
 	Imf::InputFile exr_file(filename.c_str());
 
-	// Get the number of channels in the .exr file
-	Imf::ChannelList channels = exr_file.header().channels();
+	// img height and width
+	Header header = exr_file.header();
+	int width = header.dataWindow().max.x - header.dataWindow().min.x + 1;
+	int height = header.dataWindow().max.y - header.dataWindow().min.y + 1;
 
-	// Check if the channel name is valid
-	if (!channels.findChannel(selectedChannel.c_str())){
-		throw std::invalid_argument("CPP: Invalid channel name: " + selectedChannel);
+	// Data type
+	ChannelList channels = header.channels();
+	
+	// Check if channel names are valid
+	if (!channels.findChannel(selectedChannel[0].c_str())){
+		throw std::invalid_argument("CPP: invalid channel name: " + selectedChannel[0]);			
 	}
-	//std::cout << "Valid channel " << selectedChannel << " in file " << filename << std::endl;
+	else{
+		std::cout << "CPP: valid channel found: " << selectedChannel[0].c_str() << " in the EXR : " << filename.c_str() << std::endl;
+	}
 
+	Channel channel = channels[selectedChannel[0].c_str()];
+	PixelType pixelType = channel.type;
 
-	// Get the channel data type
-	Imf::PixelType channel_type = channels.findChannel(selectedChannel.c_str())->type;
+	std::cout << "Check 1" << std::endl;
 
-	// Allocate an array to store the channel data
-	Imf::Array2D<float>data(exr_file.header().dataWindow().max.x - exr_file.header().dataWindow().min.x + 1, exr_file.header().dataWindow().max.y - exr_file.header().dataWindow().min.y + 1);
+	// Allocate a frame buffer to hold image data
+	FrameBuffer frameBuffer;
+	Array2D<float> channelData(width, height);
+	frameBuffer.insert(selectedChannel[0].c_str(), Slice(pixelType, (char*)channelData[0]  - exr_file.header().dataWindow().min.x - exr_file.header().dataWindow().min.y * channelData.width(), sizeof(float) * width, sizeof(float) * height));
 
-	// Read the channel data
-	Imf::FrameBuffer frame_buffer;
-	frame_buffer.insert(selectedChannel.c_str(), Imf::Slice(channel_type, (char*)data[0] - exr_file.header().dataWindow().min.x - exr_file.header().dataWindow().min.y * data.width(), sizeof(float) * data.width(), sizeof(float) * data.height()));
-	exr_file.setFrameBuffer(frame_buffer);
-	exr_file.readPixels(exr_file.header().dataWindow().min.y, exr_file.header().dataWindow().max.y);
+	std::cout << "Check 2" << std::endl;
 
+	exr_file.setFrameBuffer(frameBuffer);
+	std::cout << "Check 3" << std::endl;
+	//exr_file.readPixels(header.dataWindow().min.y, header.dataWindow().max.y);
+	exr_file.readPixels(0, height - 1);
+
+	std::cout << "Check 4" << std::endl;
+
+	//py::array_t<float> channelR = py::array_t<float>({channelData.height(), channelData.width()}, channelData[0]);
 	       	
+	std::cout << "Check 5" << std::endl;
+
 	// return the channel data as numpy array
-	return py::array_t<float>({data.height(), data.width()}, data[0]);	
+	return std::vector<py::array_t<float>>{py::array_t<float>({channelData.height(), channelData.width()}, channelData[0]), py::array_t<float>({channelData.height(), channelData.width()}, channelData[0]), py::array_t<float>({channelData.height(), channelData.width()}, channelData[0])};
 }
 
 PYBIND11_MODULE(loadExrChannel, m) {

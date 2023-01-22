8e6ad8f2cd87e2d8b63a3b58ad3a1fc511edba49
mteillet <martin.teillet@hotmail.fr>
2023-01-22 19:10:23 +0100

    Tring to switch to multipart exr reading in backend CPP 	modified:   loadExrChannel.cpp

--- cppModules/loadExrChannel.cpp
+++ cppModules/loadExrChannel.cpp
@@ -7,78 +7,63 @@
 #include <stdio.h>
 #include <ImathExport.h>
 #include <ImathBox.h>
+#include <OpenEXR/ImfRgba.h>
+#include <OpenEXR/ImfMultiPartInputFile.h>
+#include <OpenEXR/ImfInputPart.h>
 #include <OpenEXR/ImfFrameBuffer.h>
 #include <OpenEXR/ImfInputFile.h>
 #include <OpenEXR/ImfChannelList.h>
 #include <OpenEXR/ImfHeader.h>
 #include <OpenEXR/ImfArray.h>
-#include <OpenEXR/ImfMultiPartInputFile.h>
 
 using namespace Imf;
 using namespace Imath;
 namespace py = pybind11;
 
-std::vector<py::array_t<float>> loadExrChan(const std::string& filename, const std::vector<std::string>& selectedChannel){
-	// Open the .exr file 
-	Imf::MultiPartInputFile exr_file(filename.c_str());
+std::vector<py::array_t<float>> loadExrChan(const std::string& filename, const std::vector<std::string>& selectedChannels){
+// Open the .exr file 
+	Imf::MultiPartInputFile exrFile(filename.c_str());
 
-	// img height and width
-	Header header = exr_file.header(0);
-	int width = header.dataWindow().max.x - header.dataWindow().min.x + 1;
-	int height = header.dataWindow().max.y - header.dataWindow().min.y + 1;
+	std::vector<py::array_t<float>> result;
 
-	// Data type
-	ChannelList channels = header.channels();
-	
-	// Check if channel names are valid
-	if (!channels.findChannel(selectedChannel[0].c_str())){
-		throw std::invalid_argument("CPP: invalid channel name: " + selectedChannel[0]);			
+	for (int i = 0; i < exrFile.parts(); ++i) {
+		Imf::InputPart inputPart(exrFile, i);
+		Imf::Header header = inputPart.header();
+		int width = header.dataWindow().max.x - header.dataWindow().min.x + 1 ;
+		int height = header.dataWindow().max.y - header.dataWindow().min.y + 1;
+		Imf::ChannelList channels = inputPart.header().channels();
+		Imf::Array2D<Imf::Rgba> pixels;
+		pixels.resizeErase(inputPart.header().dataWindow().max.y - inputPart.header().dataWindow().min.y + 1, inputPart.header().dataWindow().max.x - inputPart.header().dataWindow().min.x + 1);
+		Imf::FrameBuffer frameBuffer;
+
+		// Iterate over all selected channels
+		for (const auto& channelName : selectedChannels) {
+			// Check if the channel exists in the file
+			if (Imf::Channel *channel = channels.findChannel(channelName.c_str())) {
+				// Setup frame buffer for the current channel
+				//Imf::Rgba *pixel = &(*pixels.begin());
+				frameBuffer.insert(channelName.c_str(), Imf::Slice(Imf::FLOAT, (char*)pixels, sizeof(Imf::Rgba), sizeof(Imf::Rgba) * pixels.width()));
+				inputPart.setFrameBuffer(frameBuffer);
+				inputPart.readPixels(inputPart.header().dataWindow().min.y, inputPart.header().dataWindow().max.y);
+
+				// Convert the Array2D to a numpy array
+				py::array_t<float> channelData({pixels.height(), pixels.width()});
+				auto channelDataBuffer = channelData.request();
+				float *channelDataPtr = (float *)channelDataBuffer.ptr;
+				for (int i = 0; i < pixels.height(); ++i) {
+					for (int j = 0; j < pixels.width(); j++){
+					channelDataPtr[i * pixels.width() + j] = pixels[i][j].r;
+					}
+				}
+				result.push_back(channelData);
+			} 
+			else {
+				// Throw an error if the specified channel is not found in the file
+				throw std::runtime_error("channel " + channelName + " not found in file");
+			}
+		}
 	}
-
-	Channel channel = channels[selectedChannel[0].c_str()];
-	PixelType pixelType = channel.type;
-
-	// Allocate a frame buffer to hold image data
-	size_t sampleSize = sizeof(float);
-	Array2D<half> channelDataR(height, width);
-	Array2D<half> channelDataG(height, width);
-	Array2D<half> channelDataB(height, width);
-
-	if(pixelType == HALF){
-		sampleSize = sizeof(half);
-	}
-	int pixel_base = exr_file.header(0).dataWindow().min.y* (exr_file.header(0).dataWindow().size().x+1) + exr_file.header(0).dataWindow().min.x;
-
-	FrameBuffer frameBuffer;
-	frameBuffer.insert(selectedChannel[0].c_str(), Slice(pixelType,
-			       (char*)channelDataR[0] - pixel_base * sampleSize, 
-			       sampleSize, 
-			       sampleSize * width));
-	/*frameBuffer.insert(selectedChannel[1].c_str(), Slice(pixelType,
-			       (char*)channelDataG[0] - pixel_base * sampleSize, 
-			       sampleSize, 
-			       sampleSize * width));
-	frameBuffer.insert(selectedChannel[2].c_str(), Slice(pixelType,
-			       (char*)channelDataB[0] - pixel_base * sampleSize, 
-			       sampleSize, 
-			       sampleSize * width));*/
-
-	exr_file.setFrameBuffer(frameBuffer);
-	exr_file.readPixels(header.dataWindow().min.y, header.dataWindow().max.y);
-
-	// Casting the half arrats back to float (not handled by pybind11 otherwise)
-	Array2D<float> floatDataR(height, width);
-	//Array2D<float> floatDataG(height, width);
-	//Array2D<float> floatDataB(height, width);
-	std::copy(channelDataR[0],channelDataR[0] + (height * width), floatDataR[0]);
-	//std::copy(channelDataG[0],channelDataG[0] + (height * width), floatDataG[0]);
-	//std::copy(channelDataB[0],channelDataB[0] + (height * width), floatDataB[0]);
-
-	py::array_t<float> channelR = py::array_t<float>({channelDataR.height(), channelDataR.width()}, floatDataR[0]);
-	py::array_t<float> channelG = channelR
-	py::array_t<float> channelB = channelR
-	// return the channel data as numpy array
-	return std::vector<py::array_t<float>>{channelR, channelG, channelB};
+	return result;
 }
 
 PYBIND11_MODULE(loadExrChannel, m) {
